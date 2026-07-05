92d4233c80ca390a29173318463130a2fd044f5d
PetrJust <40766942+PetrJust@users.noreply.github.com>
2026-07-05 23:14:37 +0200

    Delete app directory

--- app/(light)/layout.tsx
+++
@@ -1,5 +0,0 @@
-// Světlá poloha je od v1.1 výchozí pro celou aplikaci (blush-50 z body).
-// Skupina (light) zůstává kvůli struktuře marketingových stránek.
-export default function LightLayout({ children }: { children: React.ReactNode }) {
-  return children;
-}
